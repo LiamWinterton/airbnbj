12bd0f25bff3ca807517ee0a9b206c70e05e91f6
Liam <liam@dijitul.uk>
2021-03-16 23:46:38 +0000

    The Image component is AWFUL

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Image from 'next/image'
 import styles from "../styles/Home.module.scss"
 
 export default function Home() {
@@ -12,6 +13,11 @@ export default function Home() {
         <div className={styles.header__top}>
           <div className={styles.container}>
             <a href="#">Get the latest on our COVID-19 response</a>
+          </div>
+        </div>
+        <div className={styles.header__bottom}>
+          <div className={styles.container}>
+            <Image src="/logo-white.png" layout="intrinsic" width={290} height={60} priority={true}/>
           </div>
         </div>
       </header>
